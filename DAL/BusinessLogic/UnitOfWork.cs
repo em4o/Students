7b8c76d5d74e59be5e7df54ba5860f223a925e23
Emin Salim <emin.salim@infonetica.net>
2021-02-06 01:37:16 +0200

    start unit of work implementation for ado.net  - it will take long time :) and currently I don't have time

---
+++ DAL/BusinessLogic/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DAL.BusinessLogic
+{
+    public class UnitOfWork : IDisposable
+    {
+        private DisciplineRepository disciplineRepository;
+        private SemesterRepository semesterRepository;
+
+        public UnitOfWork ()
+        {
+            disciplineRepository = new DisciplineRepository();
+            semesterRepository = new SemesterRepository();
+        }
+
+        public void Save()
+        {
+            //context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
