d5faee0bfe4931a25b1215c3a30f8f0c2063a317
Emin Salim <emin.salim@infonetica.net>
2021-02-05 13:50:36 +0200

    Add project files.

---
+++ StudentsWebApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace StudentsWebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
